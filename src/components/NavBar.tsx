b83c0fef6d7f64afdc9ddff0a06e771de242cb27
Jeric Epon <jericepon@gmail.com>
2025-01-22 08:48:51 +0000

    hide NavBar title on smaller screens for improved responsiveness

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
       <nav className="flex justify-between items-center p-4">
         <div className="flex bg-primary text-primary-foreground items-center text-2xl">
           <img src={Pizza} className='w-[40px]' />
-          <div className='ml-2 tracking-wider font-semibold'>React Pizza Co.</div>
+          <div className='ml-2 tracking-wider font-semibold hidden md:block'>React Pizza Co.</div>
         </div>
         <Input placeholder='Search order #' className='bg-yellow-300 text-primary-foreground border-none ring-0 focus-visible:ring-transparent max-w-[400px]' />
         <ModeToggle />
